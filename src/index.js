f6fc366b92c932cd44ec318daa40cc9e4f34c61a
Hilary Osborne <h.t.osborne@gmail.com>
2019-03-18 13:04:50 +1000

    added policy checks

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 // @flow
 
+type FieldSchema = *[];
+
+type FieldRole<V> = string | ((field: BluePrint<V>) => string);
+
+type FieldScope<V> = string | ((field: BluePrint<V>) => string);
+
 type SetValidate<V> = (...validators: string[]) => BluePrint<V>;
 
 type SetSanitize<V> = (...sanitizers: string[]) => BluePrint<V>;
 
 type SetTag<V> = (...tags: string[]) => BluePrint<V>;
 
-type SetFields<V> = (
-  type: string,
-  fields: *[],
-  determiner?: () => boolean,
-) => BluePrint<V>;
-
-type SetAllow<V> = (role: string, ...scope: string[]) => BluePrint<V>;
+type SetFields<V> = (type: string, fields: *[], determiner?: () => boolean) => BluePrint<V>;
 
 type SetValue<V> = (value: V) => BluePrint<V>;
 
-type SetDeny<V> = (role: string, ...scope: string[]) => BluePrint<V>;
+type SetDefaultValue<V> = (defaultValue: V) => BluePrint<V>;
+
+type SetAllow<V> = (role: FieldRole<V>, ...scope: FieldScope<V>[]) => BluePrint<V>;
+
+type SetDeny<V> = (role: FieldRole<V>, ...scope: FieldScope<V>[]) => BluePrint<V>;
 
 type SetUse<V> = (middleware: FieldMiddleware<V>) => BluePrint<V>;
 
-type SetOptions<V> = (options: {[string]: mixed}) => BluePrint<V>;
+type SetOptions<V> = (options: { [string]: mixed }) => BluePrint<V>;
 
 type FieldMiddleware<V> = (event: string, field: BluePrint<V>) => BluePrint<V>;
 
@@ -28,19 +32,23 @@ type BluePrint<V: *> = {
   code: string,
   attributes: {
     label: null | string,
+    defaultValue?: V,
     value?: V,
     validate: Array<string>,
     sanitize: Array<string>,
     middleware: FieldMiddleware<V>[],
     fields: *[],
     tags: string[],
-    options: {[string]: mixed}
+    options: { [string]: mixed },
+    allow: Array<[FieldRole<V>, FieldScope<V>[]]>,
+    deny: Array<[FieldRole<V>, FieldScope<V>[]]>,
   },
   validate: SetValidate<V>,
   sanitize: SetSanitize<V>,
   allow: SetAllow<V>,
   deny: SetDeny<V>,
   value: SetValue<V>,
+  defaultValue: SetDefaultValue<V>,
   use: SetUse<V>,
   tag: SetTag<V>,
   fields: SetFields<V>,
@@ -51,7 +59,12 @@ function validate(...validators) {
   return { ...this, attributes: { ...this.attributes, validate: validators } };
 }
 
+function defaultValue(defaultValue) {
+  return { ...this, attributes: { ...this.attributes, defaultValue } };
+}
+
 function value(value) {
+  console.log(value, 'THIS IS ME');
   return { ...this, attributes: { ...this.attributes, value } };
 }
 
@@ -84,7 +97,7 @@ function deny(role, ...scope) {
     ...this,
     attributes: {
       ...this.attributes,
-      allow: [...(this.attributes.deny || []), [role, scope]],
+      deny: [...(this.attributes.deny || []), [role, scope]],
     },
   };
 }
@@ -110,7 +123,7 @@ function use(middleware) {
 }
 
 function options(options) {
-  return { ...this, attributes: { ...this.attributes, options  } };
+  return { ...this, attributes: { ...this.attributes, options } };
 }
 
 const field = <V: *>(code: string, label: string): BluePrint<V> => ({
@@ -122,7 +135,9 @@ const field = <V: *>(code: string, label: string): BluePrint<V> => ({
     middleware: [],
     tags: [],
     fields: [],
-    options: {}
+    options: {},
+    allow: [],
+    deny: [],
   },
   validate,
   sanitize,
@@ -132,12 +147,13 @@ const field = <V: *>(code: string, label: string): BluePrint<V> => ({
   tag,
   fields,
   value,
-  options
+  defaultValue,
+  options,
 });
 
 const schema = [
   field<string>('first_name', 'First Name')
-    .value('Hi')
+    .defaultValue('Hi')
     .tag('awesome', 'main')
     .use((event, blueprint) => {
       return blueprint;
@@ -145,7 +161,8 @@ const schema = [
     .validate('string,min:4')
     .sanitize('string')
     .deny('*', '*')
-    .allow('USER', 'R'),
+    .allow('USER', 'R')
+    .allow('DB', 'R', 'W'),
   // While populating we should spread operator
   field('phones', 'Phone Numbers').fields('collection', [
     field<string | null>('area_code', 'Area Code'),
@@ -154,14 +171,73 @@ const schema = [
   ]),
 ];
 
-console.log(schema);
+// ---------
 
-const fromDB = await populate(schema, {}, ['DB'], ['W']);
-const withUserInput = await populate(fromDB, {}, ['USER'], ['W']);
-const validated = await verify(withUserInput);
-const masked = await mask(withUserInput, ['USER'], ['R']);
+type IsAllowedSync = (field: BluePrint<*>, roles: FieldRole<*>[], scope: FieldScope<*>[]) => boolean;
 
-const toObjs = convertToObj(masked);
-const toAPI = convertToAPI(masked);
+type IsDeniedSync = (field: BluePrint<*>, roles: FieldRole<*>[], scope: FieldScope<*>[]) => boolean;
 
+const isAllowedSync: IsAllowedSync = (field, roles, scope) => {
+  const calcRoles = roles.map(role => (typeof role === 'function' ? role(field) : role));
+  const calcScope = scope.map(scope => (typeof scope === 'function' ? scope(field) : scope));
+  return field.attributes.allow.length === 0
+    ? true
+    : field.attributes.allow.reduce((curr, [role, scope]) => {
+        const fieldRole = typeof role === 'function' ? role(field) : role;
+        const fieldScope = scope.map(scope => (typeof scope === 'function' ? scope(field) : scope));
+        if (fieldRole.indexOf('*') > -1 && fieldScope.indexOf('*') > -1) {
+          return true;
+        } else if (fieldRole.indexOf('*') > -1 && fieldScope.some(scope => calcScope.indexOf(scope) > -1)) {
+          return true;
+        } else if (calcRoles.indexOf(fieldRole) > -1 && fieldScope.some(scope => calcScope.indexOf(scope) > -1)) {
+          return true;
+        } else {
+          return curr;
+        }
+      }, false);
+};
 
+const isDeniedSync: IsDeniedSync = (field, roles, scope) => {
+  const calcRoles = roles.map(role => (typeof role === 'function' ? role(field) : role));
+  const calcScope = scope.map(scope => (typeof scope === 'function' ? scope(field) : scope));
+  return field.attributes.deny.length === 0
+    ? false
+    : field.attributes.deny.reduce((curr, [role, scope]) => {
+        const fieldRole = typeof role === 'function' ? role(field) : role;
+        const fieldScope = scope.map(scope => (typeof scope === 'function' ? scope(field) : scope));
+        if (fieldRole.indexOf('*') > -1 && fieldScope.indexOf('*') > -1) {
+          return true;
+        } else if (fieldRole.indexOf('*') > -1 && fieldScope.some(scope => calcScope.indexOf(scope) > -1)) {
+          return true;
+        } else if (calcRoles.indexOf(fieldRole) > -1 && fieldScope.some(scope => calcScope.indexOf(scope) > -1)) {
+          return true;
+        } else {
+          return curr;
+        }
+      }, false);
+};
+
+type PopulateSync = (
+  schema: FieldSchema,
+  data: { [string]: mixed },
+  roles: FieldRole<*>[],
+  scope: FieldScope<*>[],
+) => FieldSchema;
+
+const populateSync: PopulateSync = (schema, data, roles, scope) =>
+  schema.map(field => {
+    if (isDeniedSync(field, roles, scope) && !isAllowedSync(field, roles, scope)) {
+      return field;
+    }
+    return {...field}.value(data[field.code]);
+  }, []);
+
+const fromDB = populateSync(schema, { first_name: 'Tom' }, ['DB'], ['W']);
+
+console.log(fromDB);
+// const withUserInput = await populate(fromDB, {}, ['USER'], ['W']);
+// const validated = await verify(withUserInput);
+// const masked = await mask(withUserInput, ['USER'], ['R']);
+//
+// const toObjs = convertToObj(masked);
+// const toAPI = convertToAPI(masked);
